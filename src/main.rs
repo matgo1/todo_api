7b071d27287fdd6cfcceb7bd9c86f659abb22654
matgo1 <mathewstaran2707@gmail.com>
2026-09-15 22:50:11 +0300

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use anyhow::Result;
+//! A minimal REST API backing a Telegram to-do bot.
+mod logic;
 use axum::{Router, routing};
 use tokio::net::TcpListener;
 
+/// Port for binding for the API
 const PORT: &str = "127.0.0.1:3001";
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
+    // Get all routers
+    let app = Router::new().route("/", routing::get(|| async { "HELLO" }));
 
+    // Binding port for the API
     let listener = TcpListener::bind(PORT).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
