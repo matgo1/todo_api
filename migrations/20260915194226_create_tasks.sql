a1899c8196d3a6503f04403fa850f4e257876d8d
matgo1 <mathewstaran2707@gmail.com>
2026-09-19 16:54:51 +0300

    Little refuctor to make title a primary key

--- migrations/20260915194226_create_tasks.sql
+++ migrations/20260915194226_create_tasks.sql
@@ -1,6 +1,5 @@
 CREATE TABLE tasks (
-    id BIGSERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
+    title TEXT NOT NULL PRIMARY KEY,
     description TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     completed_at TIMESTAMPTZ
