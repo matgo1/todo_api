1a1b856e7659820bedaae6e97110baec2bc40e92
matgo1 <mathewstaran2707@gmail.com>
2026-09-20 12:57:16 +0300

    Removed temporar testing lines

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 //! A minimal REST API backing a Telegram to-do bot.
 mod logic;
 use axum::{Router, routing};
-use logic::db;
 use tokio::net::TcpListener;
 
 /// Port for binding for the API
@@ -18,12 +17,6 @@ async fn main() {
 
     // Binding port for the API
     let listener = TcpListener::bind(PORT).await.unwrap();
-
-    let conn = &db::establish_connection().await.unwrap();
-
-    let _ = db::db_add(conn, "Jopa".to_owned(), Some("Jopa".to_owned())).await;
-    let table = db::db_read_all(conn).await;
-    println!("{:?}", table);
 
     let _ = axum::serve(listener, router).await;
 }
